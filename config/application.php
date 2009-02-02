6ad2f9272a6e5db3b873bcb4edc563a21c4e0637
Anthony Bush <anthony@anthonybush.com>
2009-02-01 21:43:07 -0600

    Aded Autoloader, SimpleReflector.

--- config/application.php
+++ config/application.php
@@ -1,7 +1,6 @@
 <?php
 
 // Derived Constants
-// (NOTE that these are not needed by LightVC, but are usually useful in the app layer.)
 define('APP_PATH', dirname(dirname(__FILE__)) . '/');
 define('WWW_BASE_PATH', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
 define('WWW_CSS_PATH', WWW_BASE_PATH . 'css/');
@@ -10,19 +9,29 @@ define('WWW_IMAGE_PATH', WWW_BASE_PATH . 'images/');
 // set_include_path(get_include_path() . PATH_SEPARATOR . APP_PATH . PATH_SEPARATOR . SHARED_PATH);
 // set_include_path(get_include_path() . PATH_SEPARATOR . APP_PATH);
 
-// Include and configure the LighVC framework
+// Include and configure the LighVC framework (http://lightvc.org/)
 include_once(APP_PATH . 'modules/lightvc/lightvc.php');
 Lvc_Config::addControllerPath(APP_PATH . 'controllers/');
 Lvc_Config::addControllerViewPath(APP_PATH . 'views/');
 Lvc_Config::addLayoutViewPath(APP_PATH . 'views/layouts/');
 Lvc_Config::addElementViewPath(APP_PATH . 'views/elements/');
 Lvc_Config::setViewClassName('AppView');
-
-// Lvc doesn't autoload the AppController, so we have to do it: (this also means we can put it anywhere)
 include(APP_PATH . 'classes/AppController.class.php');
 include(APP_PATH . 'classes/AppView.class.php');
-
-// Load Routes
 include(dirname(__FILE__) . '/routes.php');
+
+// Setup Autoloader (http://anthonybush.com/projects/autoloader/)
+include(APP_PATH . 'classes/Autoloader.class.php');
+Autoloader::setCacheFilePath(APP_PATH . 'tmp/class_path_cache.txt');
+Autoloader::excludeFolderNamesMatchingRegex('/^CVS|\..*$/');
+Autoloader::setClassPaths(array(
+	APP_PATH . 'classes/',
+	// APP_PATH . 'models/'
+));
+spl_autoload_register(array('Autoloader', 'loadClass'));
+
+// Setup SimpleReflector alias (http://anthonybush.com/projects/simplereflector/)
+// call this to debug a variable/object, e.g. jam($var);
+function jam() { call_user_func_array(array('SimpleReflector', 'jam'), func_get_args()); }
 
 ?>
