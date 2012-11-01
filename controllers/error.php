f04bc96585cc0bfdc50df53359ceff153840e71d
nanowish <>
2012-11-01 14:01:55 +0100

    Get errorNum from array with Lvc_Config::setSendActionParamsAsArray(true);

--- controllers/error.php
+++ controllers/error.php
@@ -3,12 +3,15 @@
 class ErrorController extends AppController
 {
 	public function actionView($errorNum = '404')
-	{
-		if (strpos($errorNum, '../') !== false)
-		{
-			$errorNum = '404';
+	{	
+		if(is_array($errorNum)){
+		    $errorNum = $errorNum['error'];
+		} else {
+		    if (strpos($errorNum, '../') !== false)
+		    {
+			    $errorNum = '404';
+		    }
 		}
-		
 		include_once('../classes/HttpStatusCode.class.php');
 		$statusCode = new HttpStatusCode($errorNum);
 		$this->setLayoutVar('pageTitle', $statusCode->getDefinition());
